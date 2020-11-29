f0d1a0891c629982555620a819771dad2635d098
drhtka <drhtka@gmil.com>
2020-11-29 11:59:00 +0200

    end

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Shop
  - Django==2.2
- - Pillow==7.0.0
 - DRF
 
 
 **Install**
 - pip install requirements.txt
+
+<h2>Простой блог</h2>
+Посты можно:
+ - Удалить
+- Отредактировать
+- Добавить
+Из админки, шаблона и API
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db.models import Q
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -39,5 +39,17 @@ class PostListUrlView(APIView):
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
 
+    def put(self, request, pk):
+        snippet = get_object_or_404(Mypost, pk=pk)
+        serializer = PostSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+    def delete(self, request, pk):
+        snippet = get_object_or_404(Mypost, pk=pk)
+        serializer = PostSerializer(snippet)
+        data = serializer.data
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
